bda4ccf4866ef1585a93f8fc9a6f959310d96caf
plinioaltoe <plinioaltoe@yahoo.com.br>
2019-02-24 14:32:42 -0300

    Primeiro commit do desafio 2

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'docker',
+  password: 'docker',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
